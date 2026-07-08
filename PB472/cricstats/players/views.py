5cb09a2003ee1481564c4e1806331bdd279a7a19
adit301206 <aditkapadiya1230@gmail.com>
2026-07-08 17:20:14 +0530

    Implement player listing feature with home view and template

--- PB472/cricstats/players/views.py
+++ PB472/cricstats/players/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Player
 
 # Create your views here.
+def home(request):
+    players = Player.objects.all()
+
+    return render(request , "home.html" , {"data" : players})
